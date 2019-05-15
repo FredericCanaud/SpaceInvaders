3756f0309900a19bb9e4cb0cd029f03a3b23f5a5
Frédéric Canaud <frederic.canaud@etu.unilim.fr>
2019-05-15 16:47:58 +0200

    Changer la vitesse du vaisseau

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -2,20 +2,25 @@ package fr.unilim.iut.spaceinvaders;
 
 public class Vaisseau {
 
-	Position origine;
-	Dimension dimension;
+	private Position origine;
+	private Dimension dimension;
+	private int vitesse;
 
     public Vaisseau(int longueur, int hauteur) {
 	    this(longueur, hauteur, 0, 0);
     }
 
     public Vaisseau(int longueur, int hauteur, int x, int y) {
-		   this(new Dimension(longueur, hauteur), new Position(x, y));
+		   this(new Dimension(longueur, hauteur), new Position(x, y), 1);
 	    }
 
-	public Vaisseau(Dimension dimension, Position positionOrigine) {
+	public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
 		this.dimension = dimension;
 		this.origine = positionOrigine;
+		this.vitesse = vitesse;
+	}
+	public Vaisseau(Dimension dimension, Position positionOrigine) {
+		this(dimension, positionOrigine, 1);
 	}
     public boolean occupeLaPosition(int x, int y) {
 	     if (estAbscisseCouverte(x) && estOrdonneeCouverte(y))
@@ -28,7 +33,7 @@ public class Vaisseau {
 		return (ordonneeLaPlusBasse()<=y) && (y<=ordonneeLaPlusHaute());
 	}
 
-	private int ordonneeLaPlusHaute() {
+	public int ordonneeLaPlusHaute() {
 		return this.origine.ordonnee();
 	}
 
@@ -46,11 +51,11 @@ public class Vaisseau {
 		return this.origine.abscisse()+this.dimension.longueur()-1;
 	}
 	public void seDeplacerVersLaDroite() {
-	    this.origine.changerAbscisse(this.origine.abscisse()+1);
+	    this.origine.changerAbscisse(this.origine.abscisse()+vitesse);
    }
 	
 	public void seDeplacerVersLaGauche() {
-		this.origine.changerAbscisse(this.origine.abscisse()-1);
+		this.origine.changerAbscisse(this.origine.abscisse()-vitesse);
 	}
 	public void positionner(int x, int y) {
 		  this.origine.changerAbscisse(x);
